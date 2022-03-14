f11408fe00e6b93c20778d3e56fabf6081df0e95
jeonaryeon <aryeon96@naver.com>
2022-03-14 14:28:43 +0900

    header test
    header include test

---
+++ golf/js/include.js
@@ -0,0 +1,47 @@
+$(function(){
+  $('.Hmenu').on('click', function(){
+    if($(this).hasClass('open')){
+      $('#Allmenu').removeClass('hidden');
+      $('.Hmenu').addClass('close');
+      $('.Hmenu').removeClass('open');
+      $('.close span:first-child').css({
+        transform: 'rotate(45deg)',
+        transition: 'all ease 0.4s 0s'
+      });
+      $('.close span:last-child').css({
+        transform: 'rotate(-45deg)',
+        transition: 'all ease 0.4s 0s'
+      });
+    }else{
+      $('#Allmenu').addClass('hidden');
+      $('.Hmenu').removeClass('close');
+      $('.Hmenu').addClass('open');
+      $('.open span').css({
+        transform: 'rotate(0)',
+      });
+    }
+  });
+  
+  // 모바일버전
+  var documentW = $(document).width();
+  if(documentW <= 961){
+    $('#rnb>ul>li>p').on('click', function(){
+      if($(this).next().css('display')=='none'){
+        $('#rnb .lnb').slideUp();
+        $('#rnb>ul>li>p').removeClass('current');
+        $(this).siblings('#rnb .lnb').slideDown();
+        $(this).addClass('current');
+      }else{
+        $('#rnb .lnb').slideUp();
+        $(this).removeClass('current');
+      }
+    });
+    
+    $('#rnb>ul>li>p').on('click', function(){
+      
+    });
+  }
+}); 
+
+
+
