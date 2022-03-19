7548e384a7ede8f5873a7fa163fb6e8ff7069929
jeonaryeon <aryeon96@naver.com>
2022-03-19 22:26:28 +0900

    revised css js
    revised css and js's code

--- golf/js/common.js
+++ golf/js/common.js
@@ -1,5 +1,23 @@
 $(function(){
-  
+  // scroll시 객체 보이기 
+  const saTriggerMargin = 300;
+  const saElementList = document.querySelectorAll('.ready');
+
+  const saFunc = function() {
+    for (const element of saElementList) {
+      if (!element.classList.contains('active')) { // 활성화되어 있지 않고 타겟의 위차값이 현재 위치값보다 작으면
+        if (window.innerHeight > element.getBoundingClientRect().top + saTriggerMargin) {
+          element.classList.add('active'); // 'active' class 추가
+        }
+      }
+    }
+  }
+
+  window.addEventListener('load', saFunc);
+  window.addEventListener('scroll', saFunc);
+  window.on('resize', function() { // resize시 saFunc 다시 실행
+    saFunc();
+  });
 }); 
 
 
--- golf/js/main.js
+++ golf/js/main.js
@@ -49,25 +49,8 @@ $(function(){
     });
   }); // end of award 슬라이드
     
-    
-  // scroll시 객체 보이기 
-  const saTriggerMargin = 300;
-  const saElementList = document.querySelectorAll('.ready');
-
-  const saFunc = function() {
-    for (const element of saElementList) {
-      if (!element.classList.contains('active')) { // 활성화되어 있지 않고 타겟의 위차값이 현재 위치값보다 작으면
-        if (window.innerHeight > element.getBoundingClientRect().top + saTriggerMargin) {
-          element.classList.add('active'); // 'active' class 추가
-        }
-      }
-    }
-  }
-
-  window.addEventListener('load', saFunc);
-  window.addEventListener('scroll', saFunc);
-  window.on('resize', function() { // resize시 saFunc 다시 실행
-    saFunc();
+  // resize시 #main_vision 높이값 다시 부여
+  window.on('resize', function() { 
     $('#main_vision').css({height: 'calc(100vh - 80px)'});
   });
 }); 
