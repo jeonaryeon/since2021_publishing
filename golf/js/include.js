c7e9c9eae0ecd354ccc18649ea94bab629eced25
jeonaryeon <aryeon96@naver.com>
2022-03-19 17:28:53 +0900

    scroll event and main mobile
    scroll event and main mobile

--- golf/js/include.js
+++ golf/js/include.js
@@ -3,10 +3,15 @@ $(function(){
   var documentW = $(document).width();
   if(documentW <= 961){
     $('#rnb>ul>li>p').on('click', function(){
-      $(this).parent().siblings().find('.lnb').hide();
-      $(this).siblings().toggle();
-      $(this).parent().siblings().find('p').removeClass('current');
-      $(this).toggleClass('current');
+      if(!$(this).hasClass('current')){
+        $(this).parent().siblings().find('.lnb').hide();
+        $(this).parent().siblings().find('p').removeClass('current');
+        $(this).addClass('current');
+        $(this).siblings('.lnb').show();
+      }else{
+        $(this).removeClass('current');
+        $(this).siblings('.lnb').hide();
+      }
     });
   }
 }); 
